a2958ccd55316bb1f653ffc2708e29d2a471b698
Mark Snelling <mark@bakedbeans.com>
2023-04-23 12:43:45 +0100

    Minor script changes

--- gitlab/scripts/backup_gitlab.sh
+++ gitlab/scripts/backup_gitlab.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-TOOLBOX_POD=$(kubectl get pods -n gitlab -lrelease=gitlab,app=toolbox --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}")
+BACKUP_DIR=/Users/mark/Downloads
+BACKUP_FILE=gitlab_backup.tar
+NAMESPACE=gitlab
 
-kubectl exec $TOOLBOX_POD -it -n gitlab -- backup-utility --maximum-backups 7
+TASKRUNNER_POD=$(kubectl get pods -n $NAMESPACE -lrelease=gitlab,app=toolbox --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}")
+echo "Using taskrunner pod $NAMESPACE/$TASKRUNNER_POD"
+
+kubectl exec $TASKRUNNER_POD -it -n $NAMESPACE -- backup-utility --maximum-backups 7
